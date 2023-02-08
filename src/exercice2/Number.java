21f30c192efd5a284f1b40990cd3e0e9d8b2fb31
macbook <wkoua@oxylead.net>
2023-02-08 05:59:32 +0000

    commit

---
+++ src/exercice2/Number.java
@@ -0,0 +1,23 @@
+package exercice2;
+
+/*
+*
+*@author Koua Wilfried
+*
+*/
+
+public class Number {
+    private int nombre;
+
+    public Number(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+}
